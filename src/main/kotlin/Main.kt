086535f81e9b7fa21bd638e830d625905d569dfd
Gloria-Lado <glorialadoo@gmail.com>
2023-02-02 16:38:36 +0300

    first command

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
     var  fullname = "ladoogloria"
     var age = 22
-    var phoneNumber = 0776101785
+    var phoneNumber = "0776101785"
     var wieghtinkg = 68
-    var iskenyan: Boolen =true
-    var notkenyan: Boolen =false
+    var iskenyan: Boolean =true
+    var notkenyan: Boolean =false
 
 }
